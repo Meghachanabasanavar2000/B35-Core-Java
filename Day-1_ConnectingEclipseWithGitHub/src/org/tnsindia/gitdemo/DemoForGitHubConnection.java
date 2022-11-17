0098c247d25a53a8952f3c4719ff01d3a8ed10e9
MEGHA CHANABASANAVAR <meghachanabasanavar@gmail.com>
2022-11-17 16:09:56 +0530

    first commit

---
+++ Day-1_ConnectingEclipseWithGitHub/src/org/tnsindia/gitdemo/DemoForGitHubConnection.java
@@ -0,0 +1,11 @@
+package org.tnsindia.gitdemo;
+
+public class DemoForGitHubConnection {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TNS India Foundation");
+	
+
+	}
+
+}
